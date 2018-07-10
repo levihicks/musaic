d02c79a0598e7660db77df05da0b87c8ef4024dd
Levi Hicks <lv.hicks@gmail.com>
2018-07-09 21:57:01 -0500

    create 9x9 mosaic based on user's top albums

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,16 @@ var submitButton = document.querySelector('input[name=submit]');
 var collageDiv = document.querySelector('.collage');
 var musaicCanvas = document.createElement('canvas');
 
-musaicCanvas.setAttribute('width', '300');
-musaicCanvas.setAttribute('height', '300');
+musaicCanvas.setAttribute('width', '522');
+musaicCanvas.setAttribute('height', '522');
 collageDiv.appendChild(musaicCanvas);
 var ctx = musaicCanvas.getContext('2d');
-
+var images = [];
+var imagesLoaded = 0;
 function createMusaic(){
+	imagesLoaded=0;
 	submitText = submitInput.value
-	var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + 
+	var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + 
 	submitText + '&api_key=57ee3318536b23ee81d6b27e36997cde&format=json';
 	console.log(requestURL);
 	var request = new XMLHttpRequest();
@@ -20,11 +22,29 @@ function createMusaic(){
 	request.responseType = 'json';
 	request.send();
 	request.onload = function(submitText) {
-		var image = new Image();
-	    image.src = request.response['recenttracks']['track']['0']['image']['3']['#text'];
-	    image.onload = function(){
-			ctx.drawImage(image, 0, 0);
-		};
+		var results = request.response['topalbums']['album'];
+	    for (var i = 0; i < 9; i++){
+	    	images[i] = new Image();
+	    	if (results[i]['image'][2]['#text']=="")
+	    		images[i].src="./images/notfound.png";
+	    	else
+	    		images[i].src = results[i]['image'][2]['#text'];
+	    	if (images[i].src=="")
+	    		imageLoaded();
+	    	else
+	    		images[i].onload = imageLoaded;
+	    }
 	};
-	
+}
+
+function imageLoaded(){
+	imagesLoaded+=1;
+	if (imagesLoaded == 9)
+	    drawMusaicImage();
+}
+
+function drawMusaicImage(){
+	for(var i = 0; i < 9; i++){
+	ctx.drawImage(images[i], (i % 3) * 174,  Math.floor(i/3) * 174);
+	}
 }
