8eafba3f5fd6336cb98d34caded19a879844b824
Levi Hicks <lv.hicks@gmail.com>
2018-07-11 11:42:09 -0500

    allow size change (3x3, 4x4, 5x5)

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,29 @@ var body = document.querySelector('body');
 var submitButton = document.querySelector('input[name=submit]');
 var collageDiv = document.querySelector('.collage');
 var musaicCanvas = document.createElement('canvas');
-
-musaicCanvas.setAttribute('width', '522');
-musaicCanvas.setAttribute('height', '522');
+var sizeChoice = document.querySelector('select[name=size]');
 collageDiv.appendChild(musaicCanvas);
 var ctx = musaicCanvas.getContext('2d');
 var images = [];
 var imagesLoaded;
+var imagesToLoad;
+var perRow;
 function createMusaic(){
+	imagesToLoad = sizeChoice.value;
+	perRow = Math.sqrt(imagesToLoad);
+	musaicCanvas.setAttribute('width', 174*perRow);
+	musaicCanvas.setAttribute('height', 174*perRow);
 	imagesLoaded=0;
-	submitText = submitInput.value
+	submitText = submitInput.value;
 	var requestURL = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=' + 
 	submitText + '&api_key=57ee3318536b23ee81d6b27e36997cde&format=json';
-	console.log(requestURL);
 	var request = new XMLHttpRequest();
 	request.open('GET', requestURL);
 	request.responseType = 'json';
 	request.send();
 	request.onload = function(submitText) {
 		var results = request.response['topalbums']['album'];
-	    for (var i = 0; i < 9; i++){
+	    for (var i = 0; i < imagesToLoad; i++){
 	    	images[i] = new Image();
 	    	images[i].src = (results[i]['image'][2]['#text']=="")?"./images/notfound.png":results[i]['image'][2]['#text'];
 	    	images[i].onload = imageLoaded;
@@ -33,12 +36,12 @@ function createMusaic(){
 
 function imageLoaded(){
 	imagesLoaded+=1;
-	if (imagesLoaded == 9)
+	if (imagesLoaded == imagesToLoad)
 	    drawMusaicImage();
 }
 
 function drawMusaicImage(){
-	for(var i = 0; i < 9; i++){
-		ctx.drawImage(images[i], (i % 3) * 174,  Math.floor(i/3) * 174);
+	for(var i = 0; i < imagesToLoad; i++){
+		ctx.drawImage(images[i], (i % perRow) * 174,  Math.floor(i/perRow) * 174);
 	}
 }
